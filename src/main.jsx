99e73c351fa0c7bf59e8491c93dd4f6806d847df
Aryan Mathur <65492352+aryanmathur2003@users.noreply.github.com>
2025-01-24 21:19:43 -0800

    Css Files

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 BrowserRouter
-import './index.css'
+import './css/index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
