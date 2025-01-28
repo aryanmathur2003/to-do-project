f203f24a3e3768a313d86f0534d886c2e65267e3
Aryan Mathur <65492352+aryanmathur2003@users.noreply.github.com>
2025-01-27 23:18:35 -0800

    Search Finish

--- src/services/api.js
+++ src/services/api.js
@@ -1,16 +1,18 @@
 const API_KEY = "517896c83bff0f22eba75629e398dd33"
-const BASE_URL = "https://api.themoviedb.org/3"
+const BASE_URL = "https://api.themoviedb.org/3";
 
 export const getPopularMovies = async () => {
-    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
-    console.log(response)
-    const data = await response.json()
-    console.log(data)
-    return data.results;
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
 };
 
 export const searchMovies = async (query) => {
-    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
-    const data = await response.json()
-    return data.results
-}
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
